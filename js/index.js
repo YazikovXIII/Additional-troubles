ca9f70adc6b162f2280f88efa3194a41a4b39559
tetianakhyzhniak <tanya200291@ukr.net>
2023-02-19 13:03:39 +0100

    first

--- js/index.js
+++ js/index.js
@@ -1,2 +1,16 @@
 // const arr1 = [1, 3, 6, 8, 11, 14, 19]
 // const arr2 = [2, 3, 5, 8, 9, 12, 14, 17]
+
+// Напиши скрипт который считает сумму 3 массивов
+
+// const array1 = [5, 10, 15, 20];
+// const array2 = [10, 20, 30];
+// const array3 = [5, 25, 657, 7686];
+
+// const newArray = array1.concat(array2, array3);
+// let total = 0;
+// for (let i = 0; i < newArray.length; i += 1) {
+//   total += newArray[i];
+// }
+
+// console.log(total);
